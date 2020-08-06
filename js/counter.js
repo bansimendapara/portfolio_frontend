87d29bcdf89a7fa6b272b4ad6091975abc6b854c
bansimendapara <36428790+bansimendapara@users.noreply.github.com>
2020-08-06 14:58:16 -0700

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 function updateCounter(){
-    fetch("https://g1wsmd54l1.execute-api.us-east-1.amazonaws.com/Prod/")
+    fetch("https://g1wsmd54l1.execute-api.us-east-1.amazonaws.com/Prod/visitor-count")
       .then(response => response.text())
       .then((body) => {
         var obj = JSON.parse(body);
